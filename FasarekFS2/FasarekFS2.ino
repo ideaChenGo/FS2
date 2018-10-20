bc5798533b7877fd8f6a3899390ccaaeb9ba667b
martinberlin <martin@luckycloud.de>
2018-10-20 16:52:55 +0200

    First try to get a multi-model Camera software working, dont try this at home

--- FasarekFS2/FasarekFS2.ino
+++ FasarekFS2/FasarekFS2.ino
@@ -25,11 +25,13 @@
 #include "FS2_functions.h";
 
 //Note on memorysaver selected:
+String cameraModel = "OV5642";  // OV2640
+
 // #define OV2640_MINI_2MP
 // And on Step 2: #define OV2640_CAM
-//This demo can only work on OV2640_MINI_2MP or ARDUCAM_SHIELD_V2 platform.
-#if !(defined (OV2640_MINI_2MP))
-#error Please select the hardware platform and camera module in the ../libraries/ArduCAM/memorysaver.h file
+//This demo can only work on OV2640_MINI_2MP or OV5642_MINI_5MP_PLUS
+#if !(defined (OV5642_MINI_5MP_PLUS))
+  #error Please select the hardware platform and camera module in the ../libraries/ArduCAM/memorysaver.h file
 #endif
 // set GPIO16 as the slave select :
 const int CS = 16;
@@ -245,21 +247,40 @@ Serial.println("mounted file system");
     Serial.println(F("SPI1 interface Error!"));
     while (1);
   }
-#if defined (OV2640_MINI_2MP) || defined (OV2640_CAM)
+
+// General camera settings
+//Change to JPEG capture mode and initialize the OV2640 module
+myCAM.set_format(JPEG);
+myCAM.InitCAM();
+
+if (cameraModel == "OV2640") {
   //Check if the camera module type is OV2640
   myCAM.wrSensorReg8_8(0xff, 0x01);
-  myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
-  myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
-  if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 )))
+  //myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+  //myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+  if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 ))) {
     Serial.println(F("Can't find OV2640 module!"));
-  else
+  } else {
     Serial.println(F("OV2640 detected."));
-#endif
+    myCAM.OV2640_set_JPEG_size(jpegSize); 
+  }
+}
+  if (cameraModel == "OV5642") {
+      myCAM.clear_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK); //disable low power
+  delay(100);
+//Check if the camera module type is OV5642
+  myCAM.wrSensorReg16_8(0xff, 0x01);
+  myCAM.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
+  myCAM.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
+   if((vid != 0x56) || (pid != 0x42)) {
+     Serial.println("Can't find OV5642 module!");
+   } else {
+     Serial.println("OV5642 detected.");
+     myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH
+     myCAM.OV5642_set_JPEG_size(jpegSize);
+   }
+  }
 
-  //Change to JPEG capture mode and initialize the OV2640 module
-  myCAM.set_format(JPEG);
-  myCAM.InitCAM();
-  myCAM.OV2640_set_JPEG_size(jpegSize); 
   Serial.println("JPEG_Size:"+String(jpegSize));
 
   myCAM.clear_fifo_flag();
@@ -315,8 +336,9 @@ String camCapture(ArduCAM myCAM) {
 
   myCAM.CS_LOW();
   myCAM.set_fifo_burst();
-  SPI.transfer(0xFF);
-
+  if (cameraModel == "OV2640") {
+    SPI.transfer(0xFF);
+  }
   if (client.connect(upload_host, 80) || onlineMode) { 
     if (onlineMode) {
       while(client.available()) {
