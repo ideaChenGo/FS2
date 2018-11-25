f535423269ed410c4d56600d3bdb823d53bc1207
martinberlin <martin@luckycloud.de>
2018-11-25 23:04:21 +0100

    references #9 Restore serverStream(). Make settings work again. Exposure not tested. At this point camera is ON only while taking picture and then VCC is off

--- FasarekFS2/FS2helperFunctions.h
+++ FasarekFS2/FS2helperFunctions.h
@@ -66,94 +66,6 @@ void start_capture() {
   myCAM.start_capture();
 }
 
-void serverStream() {
-    printMessage("STREAMING");
-  if (cameraModelId == 5) {
-    myCAM.OV2640_set_JPEG_size(2);
-  } else if (cameraModelId == 3) {
-    myCAM.OV5642_set_JPEG_size(1);
-  }
-  WiFiClient client = server.client();
-
-  String response = "HTTP/1.1 200 OK\r\n";
-  response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
-  server.sendContent(response);
-  int counter = 0;
-  while (true) {
-    counter++;
-    // Use a handleClient only 1 every 99 times
-    if (counter % 99 == 0) {
-       server.handleClient();
-       Serial.print(String(counter)+" % 99 Matched");
-    }
-    start_capture();
-    while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
-    size_t len = myCAM.read_fifo_length();
-
-    if (len == 0 ) //0 kb
-    {
-      Serial.println(F("Size is 0."));
-      continue;
-    }
-    myCAM.CS_LOW();
-    myCAM.set_fifo_burst();
-    if (!client.connected()) {
-      client.stop(); is_header = false; break;
-    }
-    response = "--frame\r\n";
-    response += "Content-Type: image/jpeg\r\n\r\n";
-    server.sendContent(response);
-    
-    while ( len-- )
-    {
-      temp_last = temp;
-      temp =  SPI.transfer(0x00);
-
-      //Read JPEG data from FIFO
-      if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
-      {
-        buffer[i++] = temp;  //save the last  0XD9
-        //Write the remain bytes in the buffer
-        myCAM.CS_HIGH();;
-        if (!client.connected()) {
-          client.stop(); is_header = false; break;
-        }
-        client.write(&buffer[0], i);
-        is_header = false;
-        i = 0;
-      }
-      if (is_header == true)
-      {
-        //Write image data to buffer if not full
-        if (i < bufferSize)
-          buffer[i++] = temp;
-        else
-        {
-          //Write bufferSize bytes image data to file
-          myCAM.CS_HIGH();
-          if (!client.connected()) {
-            client.stop(); is_header = false; break;
-          }
-          client.write(&buffer[0], bufferSize);
-          i = 0;
-          buffer[i++] = temp;
-          myCAM.CS_LOW();
-          myCAM.set_fifo_burst();
-        }
-      }
-      else if ((temp == 0xD8) & (temp_last == 0xFF))
-      {
-        is_header = true;
-        buffer[i++] = temp_last;
-        buffer[i++] = temp;
-      }
-    }
-    if (!client.connected()) {
-      client.stop(); is_header = false; break;
-    }
-  }
-}
-
 void serverStartTimelapse() {
     digitalWrite(ledStatusTimelapse, HIGH);
     printMessage("TIMELAPSE enabled");
@@ -181,16 +93,12 @@ void serverCameraSettings() {
      String argument  = server.argName(0);
      String setValue = server.arg(0);
      if (argument == "effect") {
-       if (cameraModelId == 5) {
-         myCAM.OV2640_set_Special_effects(setValue.toInt());
-       }
-       if (cameraModelId == 3) {
-         Serial.print(setValue);Serial.print(" in OV5642 this still does not work: https://github.com/ArduCAM/Arduino/issues/377");
-         myCAM.OV5642_set_Special_effects(setValue.toInt());
-       }
+       cameraSetting = "effect";
+       cameraSetValue= setValue.toInt();
      }
      if (argument == "exposure" && cameraModelId == 3) {
-         myCAM.OV5642_set_Exposure_level(setValue.toInt());
+        cameraSetting = "exposure";
+        cameraSetValue= setValue.toInt();
      }
      server.send(200, "text/html", "<div id='m'>"+argument+" updated to value "+setValue+"<br>See it on effect on next photo</div>"+ javascriptFadeMessage);
 }
--- FasarekFS2/FasarekFS2.ino
+++ FasarekFS2/FasarekFS2.ino
@@ -109,6 +109,8 @@ struct config_t
 byte u8cursor = 1;
 byte u8newline = 5;
 
+String cameraSetting = "effect";
+byte   cameraSetValue= 7;
 #include "FS2helperFunctions.h"; // Helper methods: printMessage + EPROM
 #include "serverFileManager.h";  // Responds to the FS Routes
 // ROUTING Definitions
@@ -611,9 +613,11 @@ void serverCapture() {
   cameraInit();
   // Set back the selected resolution
   if (cameraModelId == 5) {
+    myCAM.OV2640_set_Special_effects(cameraSetValue);
     myCAM.OV2640_set_JPEG_size(jpeg_size_id);
   } else if (cameraModelId == 3) {
     myCAM.OV5642_set_JPEG_size(jpeg_size_id);
+    myCAM.OV5642_set_Exposure_level(cameraSetValue);
   }
 
   start_capture();
@@ -825,6 +829,90 @@ void cameraOff() {
    digitalWrite(gpioCameraVcc, HIGH); // Power camera OFF
 }
 
+void serverStream() {
+  cameraInit();
+    printMessage("STREAMING");
+  if (cameraModelId == 5) {
+    myCAM.OV2640_set_JPEG_size(2);
+  } else if (cameraModelId == 3) {
+    myCAM.OV5642_set_JPEG_size(1);
+  }
+  WiFiClient client = server.client();
+
+  String response = "HTTP/1.1 200 OK\r\n";
+  response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+  server.sendContent(response);
+  int counter = 0;
+  while (true) {
+    counter++;
+    // Use a handleClient only 1 every 99 times
+    if (counter % 99 == 0) {
+       server.handleClient();
+       Serial.print(String(counter)+" % 99 Matched");
+    }
+    start_capture();
+    while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
+    size_t len = myCAM.read_fifo_length();
+
+    if (len == 0 ) //0 kb
+    {
+      Serial.println(F("Size is 0."));
+      continue;
+    }
+    myCAM.CS_LOW();
+    myCAM.set_fifo_burst();
+    response = "--frame\r\n";
+    response += "Content-Type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+    
+    while ( len-- )
+    {
+      temp_last = temp;
+      temp =  SPI.transfer(0x00);
+
+      //Read JPEG data from FIFO
+      if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
+      {
+        buffer[i++] = temp;  //save the last  0XD9
+        //Write the remain bytes in the buffer
+        myCAM.CS_HIGH();
+        client.write(&buffer[0], i);
+        is_header = false;
+        i = 0;
+      }
+      if (is_header == true)
+      {
+        //Write image data to buffer if not full
+        if (i < bufferSize)
+          buffer[i++] = temp;
+        else
+        {
+          //Write bufferSize bytes image data to file
+          myCAM.CS_HIGH();
+          if (!client.connected()) {
+            client.stop(); is_header = false; break;
+          }
+          client.write(&buffer[0], bufferSize);
+          i = 0;
+          buffer[i++] = temp;
+          myCAM.CS_LOW();
+          myCAM.set_fifo_burst();
+        }
+      }
+      else if ((temp == 0xD8) & (temp_last == 0xFF))
+      {
+        is_header = true;
+        buffer[i++] = temp_last;
+        buffer[i++] = temp;
+      }
+    }
+    if (!client.connected()) {
+      client.stop(); is_header = false; break;
+    }
+  }
+  cameraOff();
+}
+
 void loop() {
    if (onlineMode) { 
     server.handleClient(); 
@@ -837,4 +925,3 @@ void loop() {
     printMessage("> Timelapse captured");
   }
 }
-
